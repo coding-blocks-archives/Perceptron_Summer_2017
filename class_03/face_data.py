8e4bab735b3fa2b1113f1fe98bf02ffe029e2b32
shubham1810 <skd.1810@gmail.com>
2017-06-18 11:51:13 +0530

    face data collection. 01

--- class_03/face_data.py
+++ class_03/face_data.py
@@ -1,27 +1,27 @@
 # Collect face data
 
-#facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-#gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
-#	faces = facec.detectMultiScale(gray, 1.3, 5)
-#for (x,y,w,h) in faces:
-#	fc = fr[y:y+h, x:x+w, :]
-#cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
-
 import numpy as np
 import cv2
 
 cam = cv2.VideoCapture(0)
-# cam1 = cv2.VideoCapture(1)
+facec = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+data = []
 
 while True:
 	ret, fr = cam.read()
 	if ret == True:
-		
-		cv2.imshow('frame', fr)
+		gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+		faces = facec.detectMultiScale(gray, 1.3, 5)
 
+		for (x,y,w,h) in faces:
+			fc = fr[y:y+h, x:x+w, :]
+			r = cv2.resize(fc, (50, 50))
+
+			cv2.rectangle(fr, (x, y), (x+w, y+h), (0, 0, 255), 2)
+		
+		cv2.imshow('fr', fr)
 		if cv2.waitKey(1) == 27:
-		# if cv2.waitkey(1) & 0xFF == ord('q'):
 			break
 	else:
 		print "error"
